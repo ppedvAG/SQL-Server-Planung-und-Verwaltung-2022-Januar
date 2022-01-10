57ed90e10b96436cd03721cfe00d4bc9867bcb17
Rauchendraser <andreasr@ppedv.de>
2022-01-10 10:52:20 +0100

    Virtualisierung Installation

---
+++ SQL Planung und Verwaltung/SQL Planung und Verwaltung/00_Virtualisierung.sql
@@ -0,0 +1,19 @@
+--Konsolidierung
+
+--an Hardware gedacht? --> HDD!!!!!!
+
+--NUMA.. ist das die Realität
+--Socket beistzen immer einen best ANteil an RAM Slots..
+
+RAM: 16 GB
+1 Socket 
+4 Log Prozessoren
+OS : 4 GB 
+
+HV-DC					RAM															CPU Kerne
+							   2 GB dynmmisch 512 bis 2048					2
+HV-SQL1				5,5 GB fix														4 Kerne
+HV-SQL2				4,5  GB fix													4 Kerne
+
+
+
---
+++ SQL Planung und Verwaltung/SQL Planung und Verwaltung/01_Installation.sql
@@ -0,0 +1,96 @@
+/*
+
+schulung\Administrator
+ppedv2019!
+
+
+Installation
+
+
+Instanzfunktionen (Dinge, die man mehrfach installieren kann)
+Instanzen sind extrem isoliert... eig Prozess, eig RAM, eig CPU ...
+Idee für mehr Instanzen:  aus Sicherheitsgründen (Sysadmin)
+
+
+Freigegebene Funktionen
+nur einmal pro Rechner
+
+
+Dienstkonten
+SQL Server DB Engine     NT Service\mssqlserver
+
+	unbeaufsichtigte Dienstkonten.. autom Kennwortänderungen, nur lokaler Zugriff
+	--für Netzwerkzugriff : Computerkonto
+
+Volumewartungstask
+SQL Dienst darf DB Dateien automatisch selbst vergrößern ohne ausnullen zu müssen.
+-----------------------
+11111111111111111
+-----------------------
+
+
+
+SQL Agent  (jobs, Zeitplan)  
+
+SQL Browser teilt dem Client den korrekten Port für Instanzen mit
+
+SQLPC5\INSTANZ2 --> Browser(1434 UDP)--> Client--Port 42316
+SQLPC5   --- 1433 ..den kennt der Client..  Standport
+
+
+Volltextsuche  
+
+---> man kann auch DomUser Konten verwenden.. es gibt keine Anforderungen
+---allerdings wird für den kolaken ZUgriff immer das NT Service Konto verwendet!!
+
+
+Pfade für DBs
+
+Daten und Logdateien--> Idee .. sollte immer getrennte Laufwerke haben
+
+Goldene Regel: trenne Daten von Log -->immer!
+
+BackupPfad: Standardpfad
+
+
+Sicherheit:
+
+NT Auth oder gemischte Auth (NT + SQL Logins)
+
+bei gemischter Auth, dann ist der SA aktiv (Sysadmin)
+komplexes Kennwort für SA: -- später deaktivieren undn Ersatz SA KOnto anlegen SAADMIN zb
+
+
+--Windows Admins sind keine SQL Admins. --> man muss ein Windows Konto oder Grupe angeben
+
+
+
+Tempdb
+extrem wichtig für Performance..kann sehr groß werden
+sollte eig HDDs haben
+Regel: trenne Log von Daten per HDD
+Anzahl der Dateien = Anzahl der Kerne, aber max 8
+
+
+MAXDOP
+max Anzahl der Kerne pro Abfrage, = Anzahl der Kerne max 8
+früher : 0 = alle
+
+Arbeitspeicher
+
+max Wert für RAM = (Gesamt - OS)  OS ca /mind 2 GB
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
